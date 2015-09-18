79b2b835cb8091082af96d300e5e1daf8ca37b5c
markatch <choumk@gmail.com>
2015-09-18 16:10:05 -0500

    Create footer.inc.php

---
+++ includes/footer.inc.php
@@ -0,0 +1,11 @@
+<hr>
+<div style="text-align: center;">
+By Ari Chou
+</div>
+
+</div><!-- container -->
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
