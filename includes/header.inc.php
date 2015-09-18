339e14aa1e2b2d7e160a225143f244100bd193c9
markatch <choumk@gmail.com>
2015-09-18 16:09:13 -0500

    Create header.inc.php

---
+++ includes/header.inc.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require_once 'util.inc.php';
+
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ari&#39;s Dutch Stuff</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="icon" type="image/png" href="http://arichou.com/dutch/includes/ari_head.png" />
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	
+	<link rel="stylesheet" href="dutch_styles.css">
+</head>
+</head>
+<body>
+
+<div class="container">
